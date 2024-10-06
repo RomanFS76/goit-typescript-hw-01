8f52685637d36f95072d9d929009a8b0508ef5ed
RomanFS76 <korvy85@gmail.com>
2024-10-06 23:44:11 +0300

    add extends object 3.ts

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,11 @@
 import "./style.css";
 
+function merge<T extends object, K extends object>(objA: T, objB: K): T & K {
+    return Object.assign({}, objA, objB);
+  }
+  
+  const objA = { name: "Bob" };
+  const objB = { age: 47 };
+  
+  const mergedObj = merge(objA, objB);
+  console.log(mergedObj); // { name: 'Bob', age: 47 }
